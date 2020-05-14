d94ecd919afda3a695593bfc235357e896805d95
Kratu Desai <kratudesai@Kratus-MacBook-Pro.local>
2020-05-14 00:22:06 -0500

    search saved and nomatch js added form results com

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -7,3 +7,18 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+/* 
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+*/
