05a1d3e759a65a69c61ff6039555108376059af7
Kratu Desai <kratudesai@Kratus-MacBook-Pro.local>
2020-05-14 19:43:46 -0500

    gradient added

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,10 +1,12 @@
 import React from "react";
+import {appendScript} from '../../utils/appendScript'
 
 function Header() {
+    appendScript("https://desaign.app/clients/cli/javascript/gradient-czP5vBAsHbnK8R2zvetc/k9r9kd49CVdz89uGt9X4.js");
     return (
-        <div className="jumbotron text-center bg-secondary rounded-0 mt-5">
+        <div className="jumbotron text-center gradientBody rounded-0 mt-5">
             <div className="container text-light">
-                <h1>(React) Google Books Search</h1>
+                <h1>Google Book Search</h1>
                 <h5>Search for and Save Books of Interest</h5>
             </div>
         </div>
