63eb6f0c005875a08d6c5883286f8b700550caf5
Kratu Desai <kratudesai@Kratus-MacBook-Pro.local>
2020-05-20 23:39:00 -0500

    background graphic updated

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import {appendScript} from '../../utils/appendScript'
 
 function Header() {
-    appendScript("https://desaign.app/clients/cli/javascript/gradient-czP5vBAsHbnK8R2zvetc/k9r9kd49CVdz89uGt9X4.js");
+    appendScript("https://desaign.app/clients/cli/javascript/gradient-czP5vBAsHbnK8R2zvetc/Y7cYp4z625XseZBrZST2.js");
     return (
         <div className="jumbotron text-center gradientBody rounded-0 mt-5">
             <div className="container text-light">
